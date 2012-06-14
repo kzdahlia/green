6e6a4a8efb460582cdddf49fe1a9771f4321423e
Dahlia <fishball7557@Dahliade-MacBook-Pro.local>
2012-06-14 15:55:44 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Green::Application.config.secret_token = 'a80a06943d28be85f368ed0fbc175e73d0e764256cb2ce943f5c05c6a272719cfa0f60d44c64c76bf9a73ffde89878b4926a6909e53497baf9e0fce911b94105'

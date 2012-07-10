301ca815d484467dfbfc82ceb05fbf5964c3f982
marsz <marsz330@gmail.com>
2012-07-11 00:09:51 +0800

    foto checked counter and checker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def foto_file_path(foto)
     foto.has_thumb? ? foto.url_thumb : user_foto_path(foto.user, foto, :format => :jpg)
   end
+  
+  def foto_checked_number
+    raw "<span id='foto_checked'>0</span>"
+  end
 end

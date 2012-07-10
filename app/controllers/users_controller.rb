d78c8b98327616499e6f25a8834c52512a792b58
marsz <marsz330@gmail.com>
2012-07-10 13:48:10 +0800

    add dropbox syncing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,9 @@ class UsersController < ApplicationController
     response.headers['Content-Type'] = 'image/jpeg'
     render :text => current_user.dropbox_client.thumbnail("Camera%20Uploads/#{foto.filename}", 'large')
   end
+  
+  def dropbox
+    current_user.parse_dropbox_photos
+    redirect_to user_fotos_path(current_user)
+  end
 end

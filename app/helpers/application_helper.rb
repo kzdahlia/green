ca9e6f2ded0d07a03b0d5e6adcd4e32a5b20578f
Dahlia <fishball7557@Dahliade-MacBook-Pro.local>
2012-07-11 17:34:34 +0800

    單張照片footer拿掉、subbar修改...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
   def foto_checked_number
     raw "<span id='foto_checked'>0</span>"
   end
+
+  def body_classes(append_classes = nil)
+    classes = [params[:controller].gsub("/","-"), params[:action]]
+    if append_classes.is_a?(String)
+      classes << append_classes 
+    elsif append_classes.is_a?(Array)
+      classes.concat(append_classes)
+    end
+    classes.join(" ")
+  end
+  
 end
